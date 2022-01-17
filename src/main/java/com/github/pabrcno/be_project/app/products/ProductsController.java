c8e22e439abeaea93be1ff54b4d0714969edd65c
pabrcno <pabrcn@gmail.com>
2022-01-17 16:05:01 -0300

    Problems handling

--- src/main/java/com/github/pabrcno/be_project/app/products/ProductsController.java
+++ src/main/java/com/github/pabrcno/be_project/app/products/ProductsController.java
@@ -1,13 +1,9 @@
 package com.github.pabrcno.be_project.app.products;
-
 import java.util.UUID;
-
-import com.github.pabrcno.be_project.app.users.UsersController;
 import com.github.pabrcno.be_project.domain.products.IProductsDao;
 import com.github.pabrcno.be_project.domain.products.Product;
 import com.github.pabrcno.be_project.domain.users.IUsersDao;
 import com.github.pabrcno.be_project.domain.users.User;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
